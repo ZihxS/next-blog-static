95eeb841e4be62f1db32012ea3d79163983efe76
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-04-23 18:45:43 +0700

    parsing props

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,8 +1,23 @@
-const Post = () => {
+import fs from 'fs'
+
+const getContent = (slug: string) => {
+    const file = `posts/${slug}.md`
+    const content = fs.readFileSync(file, "utf8")
+
+    return content
+}
+
+const Post = (props: any) => {
+    const { slug } = props.params
+    const content = getContent(slug)
+
     return (
-        <p>
-            <h2>this is post!</h2>
-        </p>
+        <div>
+            <h2>this is post with slug: {slug}</h2>
+            <div>
+                {content}
+            </div>
+        </div>
     )
 }
 
