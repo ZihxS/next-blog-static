2815969152c0acbe937faf41e1d6cb71bd6ba0d2
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-04-25 18:43:17 +0700

    styling using tw

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer>
-            this is footer
+        <footer className="border-t border-slate-400 mt-6 py-6 text-center text-slate-400">
+            <h3>Developed by ZihxS</h3>
         </footer>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 const Header = () => {
     return (
-        <header>
-            this is header
+        <header className="text-center bg-slate-800 p-8 my-6 rounded-md">
+            <Link href="/">
+                <h1 className="text-3xl text-blue-500 font-bold">ZihxS's Blog</h1>
+            </Link>
+            <p className="text-slate-300">Welcome to my tech blog</p>
         </header>
     )
 }
