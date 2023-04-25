ecc11f7cb9671ae1fa5744e4c5d7a7f14c9db231
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-04-25 19:04:53 +0700

    update layout and style

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const PostPreview = (props: PostMetadata) => {
     return (
-        <div className="border border-slate-200 p-4 rounded-md shadow-md bg-white">
+        <div className="border border-slate-300 p-4 rounded-md shadow-md bg-sky-200">
+            <p className="text-sm text-slate-500">{props.date}</p>
             <Link href={`/${props.slug}`}>
-                <h2 className="font-bold text-violet-600 hover:underline">{props.slug}</h2>
+                <h2 className="font-bold text-sky-900 hover:underline mb-3">{props.slug}</h2>
             </Link>
-            <p className="text-sm text-slate-400">{props.subtitle}</p>
-            <p className="text-slate-700">{props.date}</p>
+            <p className="text-slate-700">{props.subtitle}</p>
         </div>
     )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="border-t border-slate-400 mt-6 py-6 text-center text-slate-400">
+        <footer className="border-t border-slate-400 mt-6 py-3 text-center text-slate-400">
             <h3>Developed by ZihxS</h3>
         </footer>
     )
